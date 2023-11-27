7c9256bc2e05710e0f84d6d3c1fa0cfc379c422f
KDT live <dnjsxo0616@gmail.com>
2023-11-27 23:44:11 +0900

    feat: ConstantsMessage enum 추가

---
+++ src/main/java/view/contants/ConstantsMessage.java
@@ -0,0 +1,18 @@
+package view.contants;
+
+public enum ConstantsMessage {
+    START_MESSAGE("재미있는 15 퍼즐!"),
+    TURN_COUNT_MESSAGE("Turn %d"),
+    FINISH_MESSAGE("축하합니다! %d턴만에 퍼즐을 완성했습니다!"),
+    INPUT_MESSAGE("숫자 입력> ");
+
+    private final String message;
+
+    ConstantsMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
