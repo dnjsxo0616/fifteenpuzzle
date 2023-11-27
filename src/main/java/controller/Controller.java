3e52ee19b9213535e953a7150e18b54df6fd5a14
KDT live <dnjsxo0616@gmail.com>
2023-11-28 02:41:37 +0900

    refactor: Controller 클래스 리팩토링
    - 게임 초기화, 진행, 완료의 메서드 분리 및 필요한 상태를 멤버 변수로 관리.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,34 +8,48 @@ import view.input.InputView;
 import view.output.OutputView;
 
 public class Controller {
+    private PlayPuzzle playPuzzle;
+    private TurnCount turnCount;
 
     public void play() {
-        int[][] board = CreatePuzzleBoard.createBoard();
-        PlayPuzzle playPuzzle = new PlayPuzzle(board);
-        TurnCount turnCount = new TurnCount();
+        initializeGame();
+        playGame();
+        finishGame();
+    }
 
+    private void initializeGame() {
+        int[][] board = CreatePuzzleBoard.createBoard();
+        playPuzzle = new PlayPuzzle(board);
+        turnCount = new TurnCount();
         OutputView.print(ConstantsMessage.START_MESSAGE);
         OutputView.printNewLine();
         OutputView.printNewLine();
+    }
 
-        while(!playPuzzle.checkPuzzle()) {
-            boolean isValidInput = false;
-            while(!isValidInput) {
-                try {
-                    turnCount.increaseCount();
-                    OutputView.println(ConstantsMessage.TURN_COUNT_MESSAGE, turnCount.getTurnCount());
+    private void playGame() {
+        while (!playPuzzle.checkPuzzle()) {
+            playTurn();
+        }
+    }
 
-                    OutputView.printPuzzleBoard(playPuzzle.getPuzzle());
-
-                    playPuzzle.moveNumber(InputView.readInput());
-                    isValidInput = true;
-                } catch (IllegalArgumentException e) {
-                    OutputView.print(e.getMessage());
-                    OutputView.printNewLine();
-                }
+    private void playTurn() {
+        boolean isValidInput = false;
+        while (!isValidInput) {
+            try {
+                turnCount.increaseCount();
+                OutputView.println(ConstantsMessage.TURN_COUNT_MESSAGE, turnCount.getTurnCount());
+                OutputView.printPuzzleBoard(playPuzzle.getPuzzle());
+                playPuzzle.moveNumber(InputView.readInput());
+                OutputView.printNewLine();
+                isValidInput = true;
+            } catch (IllegalArgumentException e) {
+                OutputView.print(e.getMessage());
+                OutputView.printNewLine();
             }
         }
+    }
 
+    private void finishGame() {
         OutputView.printPuzzleBoard(playPuzzle.getPuzzle());
         OutputView.println(ConstantsMessage.FINISH_MESSAGE, turnCount.getTurnCount());
     }
