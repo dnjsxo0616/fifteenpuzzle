8c7950c5203454cc129852027f6d17fafe3c1c4d
KDT live <dnjsxo0616@gmail.com>
2023-11-28 02:09:56 +0900

    test: 빈칸이 존재하지 않을 때 테스트

--- src/test/java/domain/PlayPuzzleTest.java
+++ src/test/java/domain/PlayPuzzleTest.java
@@ -47,4 +47,18 @@ class PlayPuzzleTest {
         assertThrows(IllegalArgumentException.class, () -> playPuzzle.moveNumber(16),
                 "유효한 숫자가 아닙니다.");
     }
+
+    @Test
+    @DisplayName("빈칸_존재하지_않는_상황_테스트")
+    void noEmptyPosition() {
+        PlayPuzzle noEmptyPlayPuzzle = new PlayPuzzle(new int[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> noEmptyPlayPuzzle.moveNumber(3),
+                "빈칸이 존재하지 않습니다.");
+    }
 }
