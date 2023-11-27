085cf5809548fe87d26e76805d803b18082fc081
KDT live <dnjsxo0616@gmail.com>
2023-11-28 00:16:16 +0900

    feat: 퍼즐 생성 출력 기능 추가

--- src/main/java/view/contants/ConstantsMessage.java
+++ src/main/java/view/contants/ConstantsMessage.java
@@ -4,7 +4,9 @@ public enum ConstantsMessage {
     START_MESSAGE("재미있는 15 퍼즐!"),
     TURN_COUNT_MESSAGE("Turn %d"),
     FINISH_MESSAGE("축하합니다! %d턴만에 퍼즐을 완성했습니다!"),
-    INPUT_MESSAGE("숫자 입력> ");
+    INPUT_MESSAGE("숫자 입력> "),
+    EMPTY_PARENTHESIS_MESSAGE("[  ]"),
+    PARENTHESIS_MESSAGE("[%2d]");
 
     private final String message;
 
--- src/main/java/view/output/OutputView.java
+++ src/main/java/view/output/OutputView.java
@@ -10,4 +10,17 @@ public class OutputView {
     public static void printNewLine() {
         System.out.println();
     }
+
+    public static void printPuzzleBoard(int[][] puzzleBoard) {
+        for (int[] numbers : puzzleBoard) {
+            for (int number : numbers) {
+                if (number == 0) {
+                    System.out.print(ConstantsMessage.EMPTY_PARENTHESIS_MESSAGE.getMessage());
+                } else {
+                    System.out.printf(ConstantsMessage.PARENTHESIS_MESSAGE.getMessage(), number);
+                }
+            }
+            printNewLine();
+        }
+    }
 }
