dc3016a0fdb8fea7133bd2d0d73866aecf449690
KDT live <dnjsxo0616@gmail.com>
2023-11-27 23:50:40 +0900

    refactor: 출력 기능 리팩터링 println -> print

--- src/main/java/view/output/OutputView.java
+++ src/main/java/view/output/OutputView.java
@@ -3,9 +3,8 @@ package view.output;
 import view.contants.ConstantsMessage;
 
 public class OutputView {
-    public static void println(ConstantsMessage constantsMessage) {
+    public static void print(ConstantsMessage constantsMessage) {
         System.out.print(constantsMessage.getMessage());
-        printNewLine();
     }
 
     public static void printNewLine() {
