6d1588e5437163ab88bfb194b1de411cf709b3ab
KDT live <dnjsxo0616@gmail.com>
2023-11-28 00:06:30 +0900

    feat: 퍼즐 생성 기능 추가

--- src/main/java/domain/CreatePuzzleBoard.java
+++ src/main/java/domain/CreatePuzzleBoard.java
@@ -6,7 +6,18 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class CreatePuzzleBoard {
+    public static int[][] createBoard() {
+        List<Integer> numbers = createNumber();
+        int[][] board = new int[4][4];
 
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                board[i][j] = numbers.remove(0);
+            }
+        }
+
+        return board;
+    }
 
     private static List<Integer> createNumber() {
         List<Integer> numbers = IntStream.rangeClosed(0, 15)
